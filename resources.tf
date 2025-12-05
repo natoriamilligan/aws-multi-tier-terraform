2a4315bad6f1f8c7345518566d29f1205bee9727
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2025-12-05 13:35:55 -0600

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -4,14 +4,17 @@ locals {
     subdomain = "www.banksie.app"
 }
 
+# Create hosted zone
 resource "aws_route53_zone" "hosted_zone" {
   name = "banksie.app"
 }
 
+# Create S3 bucket
 resource "aws_s3_bucket" "app_bucket" {
   bucket = "banksie.app"
 }
 
+# Attach bucket policy to S3 bucket for CloudFront access
 resource "aws_s3_bucket_policy" "app_bucket_policy" {
   bucket = aws_s3_bucket.app_bucket.bucket
   policy = data.aws_iam_policy_document.origin_bucket_policy.json
