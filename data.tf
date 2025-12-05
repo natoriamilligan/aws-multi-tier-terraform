6d8d8c0768d1b95ea1448a8d635bffe0174a5568
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2025-12-05 13:10:49 -0600

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,25 @@
+data "aws_iam_policy_document" "origin_bucket_policy" {
+  statement {
+    sid    = "AllowCloudFrontServicePrincipal"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.app_bucket.arn}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.s3_distribution.arn]
+    }
+  }
+}
