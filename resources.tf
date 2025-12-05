89f5b59ca0d15c0437f76dc3e4b3b71500d51828
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2025-12-05 13:32:07 -0600

    added locals and s3 bucket policy for cf access

--- resources.tf
+++ resources.tf
@@ -1,3 +1,9 @@
+locals {
+    s3_origin_id = "s3origin"
+    root_domain = "banksie.app"
+    subdomain = "www.banksie.app"
+}
+
 resource "aws_route53_zone" "hosted_zone" {
   name = "banksie.app"
 }
@@ -5,3 +11,10 @@ resource "aws_route53_zone" "hosted_zone" {
 resource "aws_s3_bucket" "app_bucket" {
   bucket = "banksie.app"
 }
+
+resource "aws_s3_bucket_policy" "app_bucket_policy" {
+  bucket = aws_s3_bucket.app_bucket.bucket
+  policy = data.aws_iam_policy_document.origin_bucket_policy.json
+}
+
+
