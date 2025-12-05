885e1a44fada7225d0ace8ab340aaae5f58abed9
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2025-12-05 15:17:57 -0600

    added resources to create and validate a TLS certificate

--- resources.tf
+++ resources.tf
@@ -20,4 +20,39 @@ resource "aws_s3_bucket_policy" "app_bucket_policy" {
   policy = data.aws_iam_policy_document.origin_bucket_policy.json
 }
 
+# Create certificate for root and subdomain
+resource "aws_acm_certificate" "domain_cert" {
+  domain_name       = "banksie.app"
+  subject_alternative_names = ["www.banksie.app"]
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Create CNAME records in hosted zone
+resource "aws_route53_record" "validation_records" {
+  for_each = {
+    for domain in aws_acm_certificate.domain_cert.domain_validation_options : domain.domain_name => {
+      name    = domain.resource_record_name
+      record  = domain.resource_record_value
+      type    = domain.resource_record_type
+      zone_id = data.aws_route53_zone.banksie_app.zone_id
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = each.value.zone_id
+}
+
+# Validate the certificate using CNAME records
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.domain_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.validation_records : record.fqdn]
+}
 
