f64862fc4f699ca3ab4feee72cd5cc54cf769dc9
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2025-12-08 11:05:58 -0600

    correct spacing and added resource for adding s3 objects

--- resources.tf
+++ resources.tf
@@ -6,129 +6,136 @@ locals {
 
 # Create hosted zone
 resource "aws_route53_zone" "hosted_zone" {
-  name = "banksie.app"
+    name = "banksie.app"
 }
 
 # Create S3 bucket
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "banksie.app"
+    bucket = "banksie.app"
+}
+
+# Upload files to S3 bucket
+resource "aws_s3_object" "upload_objects_bucket" {
+    bucket = aws_s3_bucket.app_bucket.id
+    
 }
 
 # Attach bucket policy to S3 bucket for CloudFront access
 resource "aws_s3_bucket_policy" "app_bucket_policy" {
-  bucket = aws_s3_bucket.app_bucket.bucket
-  policy = data.aws_iam_policy_document.origin_bucket_policy.json
+    bucket = aws_s3_bucket.app_bucket.bucket
+    policy = data.aws_iam_policy_document.origin_bucket_policy.json
 }
 
 # Create certificate for root and subdomain
 resource "aws_acm_certificate" "domain_cert" {
-  domain_name       = "banksie.app"
-  subject_alternative_names = ["www.banksie.app"]
-  validation_method = "DNS"
+    domain_name       = "banksie.app"
+    subject_alternative_names = ["www.banksie.app"]
+    validation_method = "DNS"
 
-  lifecycle {
-    create_before_destroy = true
-  }
+    lifecycle {
+      create_before_destroy = true
+    }
 }
 
 # Create CNAME records in hosted zone
 resource "aws_route53_record" "validation_records" {
-  for_each = {
-    for domain in aws_acm_certificate.domain_cert.domain_validation_options : domain.domain_name => {
-      name    = domain.resource_record_name
-      record  = domain.resource_record_value
-      type    = domain.resource_record_type
-      zone_id = data.aws_route53_zone.banksie_app.zone_id
+    for_each = {
+      for domain in aws_acm_certificate.domain_cert.domain_validation_options : domain.domain_name => {
+        name    = domain.resource_record_name
+        record  = domain.resource_record_value
+        type    = domain.resource_record_type
+        zone_id = data.aws_route53_zone.banksie_app.zone_id
+      }
     }
-  }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = each.value.zone_id
-}
+    allow_overwrite = true
+    name            = each.value.name
+    records         = [each.value.record]
+    ttl             = 60
+    type            = each.value.type
+    zone_id         = each.value.zone_id
+  }
 
 # Validate the certificate using CNAME records
 resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn         = aws_acm_certificate.domain_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation_records : record.fqdn]
+    certificate_arn         = aws_acm_certificate.domain_cert.arn
+    validation_record_fqdns = [for record in aws_route53_record.validation_records : record.fqdn]
 }
 
 # Create OAC for S3 bucket and CloudFront distribution
 resource "aws_cloudfront_origin_access_control" "default" {
-  name                              = "default-oac"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
+    name                              = "default-oac"
+    origin_access_control_origin_type = "s3"
+    signing_behavior                  = "always"
+    signing_protocol                  = "sigv4"
 }
 
 # Create Cloudfront distribution
 resource "aws_cloudfront_distribution" "app_distribution" {
-  origin {
-    domain_name              = aws_s3_bucket.app_bucket.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
-    origin_id                = local.s3_origin_id
-  }
+    origin {
+      domain_name              = aws_s3_bucket.app_bucket.bucket_regional_domain_name
+      origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+      origin_id                = local.s3_origin_id
+    }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
+    enabled             = true
+    is_ipv6_enabled     = true
+    default_root_object = "index.html"
   
-  custom_error_response {
-  error_code            = 404
-  response_code         = 200
-  response_page_path    = "/index.html"
-  error_caching_min_ttl = 0
-}
-
-  aliases = [local.root_domain, local.subdomain]
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
+    custom_error_response {
+        error_code            = 404
+        response_code         = 200
+        response_page_path    = "/index.html"
+        error_caching_min_ttl = 0
     }
 
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
+    aliases = [local.root_domain, local.subdomain]
 
-  price_class = "PriceClass_All"
+    default_cache_behavior {
+        allowed_methods  = ["GET", "HEAD"]
+        cached_methods   = ["GET", "HEAD"]
+        target_origin_id = local.s3_origin_id
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-      locations        = []
+        forwarded_values {
+          query_string = false
+
+          cookies {
+            forward = "none"
+          }
+        }
+
+        viewer_protocol_policy = "allow-all"
+        min_ttl                = 0
+        default_ttl            = 3600
+        max_ttl                = 86400
     }
-  }
 
-  viewer_certificate {
-    acm_certificate_arn = data.aws_acm_certificate.banksie_app_cert.arn
-    ssl_support_method  = "sni-only"
-  }
+    price_class = "PriceClass_All"
+
+    restrictions {
+        geo_restriction {
+          restriction_type = "none"
+          locations        = []
+        }
+    }  
+
+    viewer_certificate {
+      acm_certificate_arn = data.aws_acm_certificate.banksie_app_cert.arn
+      ssl_support_method  = "sni-only"
+    }
 }
 
 # Create A records pointing to the CloudFront distribution
 resource "aws_route53_record" "cloudfront" {
-  for_each = aws_cloudfront_distribution.cf_distribution.aliases
-  zone_id  = data.aws_route53_zone.banksie_app.zone_id
-  name     = each.value
-  type     = "A"
+    for_each = aws_cloudfront_distribution.cf_distribution.aliases
+    zone_id  = data.aws_route53_zone.banksie_app.zone_id
+    name     = each.value
+    type     = "A"
 
-  alias {
-    name                   = aws_cloudfront_distribution.cf_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.cf_distribution.hosted_zone_id
-    evaluate_target_health = false
+    alias {
+        name                   = aws_cloudfront_distribution.cf_distribution.domain_name
+        zone_id                = aws_cloudfront_distribution.cf_distribution.hosted_zone_id
+        evaluate_target_health = false
+    }
   }
-}
+  
