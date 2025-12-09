dd2fc4a60a0abfe9dfe021a0e36d7370b48576a6
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2025-12-09 11:51:17 -0600

    complete task definition resource

--- resources.tf
+++ resources.tf
@@ -245,30 +245,24 @@ resource "aws_iam_role_policy_attachment" "AWS_managed_task_policy" {
 
 # Create task definition
 resource "aws_ecs_task_definition" "app_task" {
-  family = "app-task"
-  container_definitions = jsonencode([
-    {
-      name      = "first"
-      image     = "service-first"
-      cpu       = 10
-      memory    = 512
-      essential = true
-      portMappings = [
+    family = "app-task"
+    execution_role_arn = aws_iam_role.task_execution_role.arn
+    network_mode = "awsvpc"
+    requires_compatibilities = ["FARGATE"]
+    cpu = "512"
+    memory = "2048"
+    container_definitions = jsonencode([
         {
-          containerPort = 80
-          hostPort      = 80
+            name      = "app-container"
+            image     = "${aws_caller_identity.current.account.id}.dkr.ecr.${var.region}.amazonaws.com/${aws_ecr_repository.name}:latest"
+            cpu       = 0
+            essential = true
+            portMappings = [
+                {
+                    containerPort = 80
+                    hostPort      = 80
+                }
+            ]
         }
-      ]
-    }
-  ])
-
-  volume {
-    name      = "service-storage"
-    host_path = "/ecs/service-storage"
-  }
-
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-  }
+    ])
 }
