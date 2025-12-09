1cc1567656587677199f28d90fbf7ae780ccef0d
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2025-12-09 13:23:20 -0600

    created public subnets

--- resources.tf
+++ resources.tf
@@ -148,17 +148,32 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Create subnets
-resource "aws_subnet" "subnet_a" {
+# Create private subnets
+resource "aws_subnet" "private_a" {
     vpc_id            = aws_vpc.main.id
     cidr_block        = "10.0.1.0/24"
     availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "subnet_b" {
+resource "aws_subnet" "private_b" {
     vpc_id            = aws_vpc.main.id
     cidr_block        = "10.0.2.0/24"
     availability_zone = "us-east-1b"
+}
+
+# Create public subnets
+resource "aws_subnet" "public_a" {
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = "10.0.101.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_b" {
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = "10.0.102.0/24"
+    availability_zone = "us-east-1b"
+    map_public_ip_on_launch = true
 }
 
 # Create RDS security group
