8940578827538364f698613526deeadbe71d9288
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2025-12-10 14:31:23 -0600

    fixed name typo

--- data.tf
+++ data.tf
@@ -20,7 +20,7 @@ data "aws_iam_policy_document" "origin_bucket_policy" {
         condition {
             test     = "StringEquals"
             variable = "AWS:SourceArn"
-            values   = [aws_cloudfront_distribution.s3_distribution.arn]
+            values   = [aws_cloudfront_distribution.app_distribution.arn]
         }
     }
 }
