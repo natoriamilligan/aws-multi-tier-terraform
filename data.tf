0149ea6d5fd11a2a91c57177f4038e55130b84d5
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2025-12-10 13:56:07 -0600

    changed local name of hosted zone

--- data.tf
+++ data.tf
@@ -26,7 +26,7 @@ data "aws_iam_policy_document" "origin_bucket_policy" {
 }
 
 # Provides details about the hosted zone
-data "aws_route53_zone" "banksie_app" {
+data "aws_route53_zone" "hosted_zone" {
     name         = "banksie.app"
     private_zone = false
 }
